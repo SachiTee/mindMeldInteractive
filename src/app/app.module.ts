00be128114deb780d1eedb26e845bd68704cab5b
Sachi <stakasawa@rioseo.com>
2018-02-14 13:15:38 -0800

    creating circle progress defaults

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,18 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     // Specify ng-circle-progress as an import
     NgCircleProgressModule.forRoot({
       // set defaults here
-      radius: 100,
-      outerStrokeWidth: 16,
-      innerStrokeWidth: 8,
-      outerStrokeColor: "#78C000",
-      innerStrokeColor: "#C7E596",
-      animationDuration: 300,
+      radius: 60,
+      space: -10,
+      outerStrokeWidth: 10,
+      outerStrokeColor: "#4882c2",
+      innerStrokeColor: "#e7e8ea",
+      innerStrokeWidth: 10,
+      title: "Completion Date: 4/19/2018",
+      animateTitle: false,
+      animationDuration: 1000,
+      showUnits: false,
+      showBackground: false,
+      clockwise: false
     })
   ],
   providers: [],
